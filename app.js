c3e37addc967ea414e06f44ca0f8830b9b66ff83
akashvinchankar <86399324+akashvinchankar@users.noreply.github.com>
2021-12-02 15:43:07 +0530

    api integrattion bananaa

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input 
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+}
+
 
 function clickHandler(){
-    outputDiv.innerText = "Aaabababaaababa  " + txtInput.value;
+    var inputText = txtInput.value;   // input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(reponse => reponse.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;   // output
+        })
+        .catch(errorHandler)
 };
 
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler)
